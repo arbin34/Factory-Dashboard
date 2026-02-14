68e8e2aee580347c0bc868d4f05e4832bc71374c
arbin34 <kumarverman76@gmail.com>
2026-02-14 20:10:10 +0530

    Initial commit: add frontend and backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+let backendURL = process.env.BACKEND_URL || "http://localhost:5000";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: `${backendURL}/api`
 });
 
 export const getDashboard = () => API.get("/dashboard");
